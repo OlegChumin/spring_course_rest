898f632f887dda17054aaa61a618592ab729b0c3
OlegChumin <tschumin.oleg@gmail.com>
2023-05-28 00:41:06 +0300

    Add new files

--- src/main/java/com/zaurtregulov/spring/rest/configuration/MyConfig.java
+++ src/main/java/com/zaurtregulov/spring/rest/configuration/MyConfig.java
@@ -4,7 +4,9 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
@@ -14,6 +16,7 @@ import java.util.Properties;
 @Configuration
 @ComponentScan("com.zaurtregulov.spring.rest")
 @EnableWebMvc
+@EnableTransactionManagement
 public class MyConfig {
     @Bean
     public DataSource dataSource() {
@@ -39,5 +42,12 @@ public class MyConfig {
         hibernateProperties.setProperty("hibernate.show_sql", "true");
         sessionFactory.setHibernateProperties(hibernateProperties);
         return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory((sessionFactory().getObject()));
+        return transactionManager;
     }
 }
